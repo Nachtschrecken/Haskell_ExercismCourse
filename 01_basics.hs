099f989e694069713140c1fc17a2b25ac217fcef
Nachtschrecken <ferris@uldagon.com>
2022-08-30 13:51:00 +0200

    changes

--- 01_basics.hs
+++ 01_basics.hs
@@ -1,10 +1,13 @@
-module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
+module Basics (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
 
 -- TODO: define the expectedMinutesInOven constant
+expectedMinutesInOven :: Integer
 expectedMinutesInOven = 40
 
 -- TODO: define the preparationTimeInMinutes function
+preparationTimeInMinutes :: Num a => a -> a
 preparationTimeInMinutes layers = layers * 2
 
 -- TODO: define the elapsedTimeInMinutes function
+elapsedTimeInMinutes :: Num a => a -> a -> a
 elapsedTimeInMinutes expectedMinutesInOven preparationTimeInMinutes = expectedMinutesInOven + preparationTimeInMinutes
